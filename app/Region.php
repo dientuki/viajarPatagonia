ef490862b5f100df05bc4fcd5aa86f818ea73e0c
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-09-30 12:38:24 -0300

    added edit things

--- app/Region.php
+++ app/Region.php
@@ -18,5 +18,33 @@ class Region extends Model
      *
      * @var bool
      */
-    public $timestamps = false;    
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['region'];    
+
+    static function getEdit($id){
+
+        $result = Region::select('id', 'region')
+            ->where('id', $id)
+            ->get()->first();
+    
+        if (is_array($id)) {
+          if (count($result) == count(array_unique($id))) {
+            return $result;
+          }
+        } elseif (! is_null($result)) {
+          return $result;
+        }
+    
+        //Laravel 4 fallback
+        return abort(404);
+    
+        //throw (new ModelNotFoundException)->setModel(get_class($this->model));
+      }
+    
 }
