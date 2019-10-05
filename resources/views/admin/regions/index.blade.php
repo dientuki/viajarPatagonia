7185ceb55d1ee65a5f209addd51359cb4abf8ded
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-05 16:47:14 -0300

    more pretty screens

--- resources/views/admin/regions/index.blade.php
+++ resources/views/admin/regions/index.blade.php
@@ -2,39 +2,42 @@
 
 @section ('content')
 
-    @if (isset($regions))
-    <table class="list-table" id="list-table">
-        <thead class="hidden">
-            <tr>
-                <th>Region</th>
-                <th class="column-action">Accion</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($regions as $region)
-            <tr>
-                <td>{{ $region->region }}</td>
+@if (isset($regions))
+<table class="table table-striped table-bordered table-hover table-sm">
+    <thead class="thead-dark">
+        <tr>
+            <th>Region</th>
+            <th class="column-action">Accion</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($regions as $region)
+        <tr>
+            <td>{{ $region->region }}</td>
 
-                <td class="column-action">
-                    <a href="{{route('admin.regions.edit', $region->id)}}" class="btn btn-primary btn-sm" role="button"
-                        title="{{__('buttons.edit')}} {{ $region->region }}">{{__('buttons.edit')}}</a>
-              {!! Form::open(array('route' => array('admin.regions.destroy', $region->id), 'method' => 'DELETE', 'class' => 'form-inline')) !!}
-                <button class="modalDelete" id="button-{{ $region->id }}" type="submit" class="btn btn-danger btn-sm" title="{{__('buttons.delete')}} {{ $region->region }}" data-toggle="modal" data-target="#deleteModal">
-                  <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> {{__('buttons.delete') }}</button>
-              {!! Form::close() !!}
+            <td class="column-action px-4">
+                <div class="row">
+                <a href="{{route('admin.regions.edit', $region->id)}}" class="btn btn-primary col" title="{{__('buttons.edit')}} {{ $region->region }}">{{__('buttons.edit')}}</a>
+                
+                {!! Form::open(array('route' => array('admin.regions.destroy', $region->id), 'method' => 'DELETE', 'class' => 'col')) !!}
+                <button id="button-{{ $region->id }}" type="submit" class="btn btn-danger modalDelete"
+                    title="{{__('buttons.delete')}} {{ $region->region }}" data-toggle="modal"
+                    data-target="#deleteModal">
+                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                    {{__('buttons.delete') }}</button>
+                {!! Form::close() !!}
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
-                </td>
+@include ('admin/widgets/modal')
 
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    @include ('admin/widgets/modal')
-
-    <a href="{{route('admin.regions.create')}}">Crear</a>
-    @else
-    Enhorabuena! No hay usuarios para activar!
-    @endif
+<a href="{{route('admin.regions.create')}}" class="btn btn-primary" title="{{__('buttons.create')}} Region">{{__('buttons.create')}} Region</a>
+@else
+Enhorabuena! No hay usuarios para activar!
+@endif
 
 @endsection
