216051d5b53a8999feadc42fdc9924866a65e640
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-05 12:35:06 -0300

    added some boostrap things

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,3 +1,7 @@
 import {toogleNav} from './modules/toogleNav/toogleNav';
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/modal';
+import 'jquery';
+//import 'popper.js';
 
 toogleNav();
