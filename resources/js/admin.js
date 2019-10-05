ffe9bc466d3a796729b1b13abf27d2021c64a0ef
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-05 20:15:43 -0300

    added admin js

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,9 @@
 import {toogleNav} from './modules/toogleNav/toogleNav';
 import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/modal';
-import 'jquery';
+import $ from 'jquery';
 //import 'popper.js';
+
 
 toogleNav();
 
@@ -12,4 +13,52 @@ $('.modalDelete').on('shown.bs.modal', function () {
   })
   */
 
- $('.modalDelete').modal()
+ //$('#button-1').modal()
+
+ window.$ = $;
+
+ function test(event) {
+    if (event.target.dataset.send === undefined) {
+        console.log('preveni', event.target.dataset)
+        document.querySelector('#deleteModal .btn-danger').dataset.id = event.target.dataset.id
+        $('#deleteModal').modal('show');
+        event.preventDefault();    
+    } else {
+        console.log('asdfasdf');
+    }
+ }
+
+ Array.from(document.querySelectorAll('.modalOpener')).forEach((form) => {
+    form.addEventListener('submit', test);
+ });
+
+ /*
+ Array.from(document.querySelectorAll('.modalOpener')).forEach((form) => {
+    form.addEventListener('submit', (event) => {
+        if (event.target.dataset.send === undefined) {
+            document.querySelector('#deleteModal .btn-danger').dataset.id = event.target.dataset.id
+            $('#deleteModal').modal('show');
+            event.preventDefault();
+        }
+    });
+  });
+*/
+
+document.querySelector('#deleteModal .btn-danger').addEventListener('click', (event) => {
+  document.querySelector(`form.id-${event.target.dataset.id}`).dataset.send = true;
+  document.querySelector(`form.id-${event.target.dataset.id}`).removeEventListener('submit', test);
+  $('#deleteModal').modal('hide');
+});
+
+$("#deleteModal").on("hidden.bs.modal", function () {
+    const form = document.querySelector('form[data-send="true"]');
+
+    console.log(form)
+
+    if (form !== null) {
+        window.requestAnimationFrame(() => {
+            console.log(form)
+            form.dispatchEvent(new Event('submit'));
+        });
+    }
+});
