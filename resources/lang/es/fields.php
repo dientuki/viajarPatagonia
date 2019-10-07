6450bf5a9ade0419323e528fafa258670bd81df6
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-06 23:13:14 -0300

    added translation

---
+++ resources/lang/es/fields.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    // Region
+    'region' => 'region',
+
+    // Destino
+    'destination' => 'destino',
+
+    // Moneda
+    'sign' => 'signo',
+    'code' => 'código',
+    'currency' => 'moneda',
+    'amount' => 'cambio'            
+
+];
