cb3588f8299537dc97d6fc878f565b4e6334a4e5
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-19 16:31:52 -0300

    update medialibrary config

--- config/medialibrary.php
+++ config/medialibrary.php
@@ -96,6 +96,7 @@ return [
         Spatie\ImageOptimizer\Optimizers\Jpegoptim::class => [
             '--strip-all', // this strips out all text information such as comments and EXIF data
             '--all-progressive', // this will make sure the resulting image is a progressive one
+            '-m85', // this will store the image with 85% quality
         ],
         Spatie\ImageOptimizer\Optimizers\Pngquant::class => [
             '--force', // required parameter for this package
