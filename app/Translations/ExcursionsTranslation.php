40b9ba0f3f10c95886b7706709ca88756f3e6fd9
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-26 16:37:25 -0300

    added models

---
+++ app/Translations/ExcursionsTranslation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Translations;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExcursionsTranslation extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'excursions_translation';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['fk_language', 'fk_excursion', 'name', 'summary', 'body'];    
+
+    static function getLists() {
+      //return Region::orderBy('region')->pluck('region', 'id');
+    }
+
+    static function getEdit($id){
+
+      return CruiseshipsTranslation::select('id', 'fk_language', 'name', 'summary', 'body')
+        ->where('fk_excursion', $id)
+        ->orderBy('fk_language')
+        ->get();
+    }    
+
+    static function getUpdate($where){
+
+      return CruiseshipsTranslation::select('id', 'fk_language', 'name', 'summary', 'body')
+        ->where($where)
+        ->get()->first();
+    }    
+}
