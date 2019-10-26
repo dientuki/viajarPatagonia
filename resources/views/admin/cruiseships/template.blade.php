a4d261e9e29c72810979e7f6621b74fea0059858
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-26 09:59:40 -0300

    better look to the biggest form ever

--- resources/views/admin/cruiseships/template.blade.php
+++ resources/views/admin/cruiseships/template.blade.php
@@ -1,31 +1,38 @@
 <template class="template-dropzone">
 
-    <div class="file-row template">
-    <!-- This is used as the file preview template -->
-    <input type="text" name="images[]" value="" />
-    <div>
-        <span class="preview"><img class="thumbnail" data-dz-thumbnail /></span>
+  <li class="file-row template image-row">
+
+    <input type="hidden" name="images[]" value="" />
+    <div class="col-image" >
+      <div class="preview aspect-1-1">
+        <img class="thumbnail " data-dz-thumbnail />
+      </div>
     </div>
-    <div>
-        <p class="name" data-dz-name></p>
-        <strong class="error text-danger" data-dz-errormessage></strong>
-    </div>
-    <div>
-        <p class="size" data-dz-size></p>
-        <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-            <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+
+    <div class="col">
+      <div class="row">
+        <p class="name col" data-dz-name></p>
+        <p class="size col" data-dz-size></p>
+      </div>
+      <div>
+        <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100"
+          aria-valuenow="0">
+          <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+          <strong class="error text-danger" data-dz-errormessage></strong>
         </div>
+      </div>
     </div>
-    <div>
-        <div data-dz-remove class="btn btn-warning cancel">
-            <i class="glyphicon glyphicon-ban-circle"></i>
-            <span>Cancel</span>
-        </div>
-        <div data-dz-remove class="btn btn-danger delete">
+
+    <div class="col-action">
+      <div data-dz-remove class="btn btn-warning cancel">
+        <i class="glyphicon glyphicon-ban-circle"></i>
+        <span>Cancel</span>
+      </div>
+      <div data-dz-remove class="btn btn-danger delete">
         <i class="glyphicon glyphicon-trash"></i>
         <span>Delete</span>
-        </div>
+      </div>
     </div>
-    </div>
+  </li>
 
 </template>
