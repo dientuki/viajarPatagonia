32135a9fd65fb48eb30987f5b694fdf72b1de242
Juan "Dientuki" Farias <dientuki@gmail.com>
2019-10-28 09:50:13 -0300

    header

--- resources/views/front/main-parts/header.blade.php
+++ resources/views/front/main-parts/header.blade.php
@@ -6,44 +6,46 @@ use App\Translations\Language;
 ?>
 
 <header id="header">
-  <a href="#">Viajar por patagonia</a>
+  <div class="wrapper">
+    <a href="#">Viajar por patagonia</a>
 
-  <nav>
-    <ul>
-      <li>
-        <a href="#">Inicio</div>
-      </li>
-      <li>
-        <a href="#">Hoteles</a>
-      </li>
-      <li>
-        <a href="#">Autos</a>
-      </li>
-    </ul>
-  </nav>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Hoteles</a>
+        </li>
+        <li>
+          <a href="#">Autos</a>
+        </li>
+      </ul>
+    </nav>
 
-  <form action="#">
-    <input />
-  </form>
+    <form action="#">
+      <input />
+    </form>
 
-  <div>
-    <div>Moneda</div>
-    <?php $currencies = Currency::getAll(); ?>
-    <ul>
-      @foreach ($currencies as $currency)
-        <li>{{ $currency->sign }} {{ $currency->iso }}</li>
-      @endforeach
-    </ul>
+    <div>
+      <div>Moneda</div>
+      <?php $currencies = Currency::getAll(); ?>
+      <ul>
+        @foreach ($currencies as $currency)
+          <li>{{ $currency->sign }} {{ $currency->iso }}</li>
+        @endforeach
+      </ul>
+    </div>
+
+    <div>
+      <div>Idioma</div>
+      <?php $languages = Language::getAll(); ?>
+      <ul>
+        @foreach ($languages as $language)
+          <li>{{ $language->language }}</li>
+        @endforeach
+      </ul>
+    </div>
   </div>
-
-  <div>
-    <div>Idioma</div>
-    <?php $languages = Language::getAll(); ?>
-    <ul>
-      @foreach ($languages as $language)
-        <li>{{ $language->language }}</li>
-      @endforeach
-    </ul>
-  </div>
-
+  
 </header>
