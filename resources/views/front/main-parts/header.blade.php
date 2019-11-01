7f59461000cc755c770b2820fa62a0422c583a60
Juan <dientuki@gmail.com>
2019-11-01 15:53:19 -0300

    header first aproach

--- resources/views/front/main-parts/header.blade.php
+++ resources/views/front/main-parts/header.blade.php
@@ -5,11 +5,11 @@ use App\Translations\Language;
 
 ?>
 
-<header id="header">
+<header class="header">
   <div class="wrapper">
     <a class="header__logo text-hidden" href="#" title="{{ __('front.go_to_home') }}">Viajar por patagonia</a>
 
-    <nav id="navigation">
+    <nav class="navigation">
       <ul class="navigation__ul">
         <li class="navigation__li">
           <a class="navigation__link" href="#">{{ ucfirst(__('front.home')) }}</a>
@@ -23,14 +23,14 @@ use App\Translations\Language;
       </ul>
     </nav>
 
-    <form action="#" id="search_form">
-      <input type="search" placeholder="{{ ucfirst(__('front.search')) }}" />
-      <button>{!! load_svg('ico-search') !!}</button>
+    <form action="#" class="search_form">
+      <input class="search_form__input" type="search" placeholder="{{ ucfirst(__('front.search')) }}" />
+      <button class="search_form__submit">{!! load_svg('ico-search') !!}</button>
     </form>
 
     <?php $currencies = Currency::getAll(); ?>
     <div class="selector">
-      <div class="selector--current">{{ ucfirst(__('front.currency')) }}{!! load_svg('ico-down') !!}</div>
+      <div class="selector--current">{!! load_svg('lang-pt') !!}{{ ucfirst(__('front.currency')) }}{!! load_svg('ico-down') !!}</div>
       <ul class="selector__ul">
         @foreach ($currencies as $currency)
           <li class="selector__li" title="{{ ucfirst(__('front.change_to')) }} {{ __('front.currency') }} {{ $currency->currency }}">{{ $currency->sign }} {{ $currency->iso }}</li>
@@ -40,7 +40,7 @@ use App\Translations\Language;
 
     <?php $languages = Language::getAll(); ?>
     <div class="selector">
-      <div class="selector--current">{{ ucfirst(__('front.language')) }}{!! load_svg('ico-down') !!}</div>
+      <div class="selector--current">{!! load_svg('lang-es') !!}{{ ucfirst(__('front.language')) }}{!! load_svg('ico-down') !!}</div>
       <ul class="selector__ul">
         @foreach ($languages as $language)
           <li class="selector__li" title="{{ ucfirst(__('front.change_to')) }} {{ __('front.language') }} {{ $language->language }}">{{ $language->language }}</li>
