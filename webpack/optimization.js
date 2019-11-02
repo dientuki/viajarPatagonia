74ee28fc014d14efdc7b7d8b768c8b743ff72a5e
Juan <dientuki@gmail.com>
2019-11-02 10:54:48 -0300

    try to optimize webpack

--- webpack/optimization.js
+++ webpack/optimization.js
@@ -2,41 +2,46 @@ const optimization = {};
 const minimizer = [];
 
 optimization['splitChunks'] = {
-    chunks: 'all',
-    cacheGroups: {
-      default: false,
-      vendors: false,
-    }
-  };
-
-  if (process.env.NODE_ENV == 'production') {
-    const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-    const TerserPlugin = require('terser-webpack-plugin');
-
-    optimization['minimize'] = true;
-
-    minimizer.push(
-      new TerserPlugin({
-        sourceMap: false,
-        terserOptions: {
-          ecma: 6,
-          output: {beautify: false},
-          compress: {
-            unused: true,
-            dead_code: true
-          },
-          warnings: false,
-          mangle: true,
-          ie8: false,
-          safari10: false
-        }
-      })
-    );
-    
-    minimizer.push(new OptimizeCSSAssetsPlugin({}));
-    
-    optimization['minimizer'] = minimizer;
-
+  chunks: 'all',
+  cacheGroups: {
+    default: false,
+    vendors: false,
   }
+};
+
+if (process.env.NODE_ENV == 'production') {
+  const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+  const TerserPlugin = require('terser-webpack-plugin');
+
+  optimization['minimize'] = true;
+
+  minimizer.push(
+    new TerserPlugin({
+      sourceMap: false,
+      terserOptions: {
+        ecma: 6,
+        output: { beautify: false },
+        compress: {
+          unused: true,
+          dead_code: true
+        },
+        warnings: false,
+        mangle: true,
+        ie8: false,
+        safari10: false
+      }
+    })
+  );
+
+  minimizer.push(new OptimizeCSSAssetsPlugin({}));
+
+  optimization['minimizer'] = minimizer;
+
+}
+
+if (process.env.NODE_ENV == 'development') {
+  optimization['removeAvailableModules'] = false;
+  optimization['removeEmptyChunks'] = false;
+}
 
 module.exports = optimization;
