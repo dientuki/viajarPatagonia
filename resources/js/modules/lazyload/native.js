41422037278e28aab430cdddc4dfe66c9b4ba9a6
Juan <dientuki@gmail.com>
2019-11-02 19:10:45 -0300

    fix lint

--- resources/js/modules/lazyload/native.js
+++ resources/js/modules/lazyload/native.js
@@ -2,23 +2,18 @@ export default class native {
 
   static init(settings) {
     document.addEventListener('DOMContentLoaded', () => {
-      const lazyImages = [].slice.call(document.querySelectorAll(settings.elements_selector));
+      const lazyImageObserver = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              const lazyImage = entry.target;
 
-      console.log(lazyImages);
-
-      let lazyImageObserver = new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const lazyImage = entry.target;
-
-            console.log(settings)
-
-            lazyImage.src = lazyImage.dataset[settings.data_src];
-            lazyImage.classList.remove(settings.elements_selector.replace(/^(.|#)/, ''));
-            lazyImageObserver.unobserve(lazyImage);
-          }
-        });
-      });
+              lazyImage.src = lazyImage.dataset[settings.data_src];
+              lazyImage.classList.remove(settings.elements_selector.replace(/^(.|#)/, ''));
+              lazyImageObserver.unobserve(lazyImage);
+            }
+          });
+        }),
+        lazyImages = [].slice.call(document.querySelectorAll(settings.elements_selector));
 
       lazyImages.forEach((lazyImage) => {
         lazyImageObserver.observe(lazyImage);
