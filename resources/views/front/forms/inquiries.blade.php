ce50bb89bc44a5b093c9ad3328a4ef45d41a65cb
Juan <dientuki@gmail.com>
2019-11-03 17:53:35 -0300

    visually done

--- resources/views/front/forms/inquiries.blade.php
+++ resources/views/front/forms/inquiries.blade.php
@@ -1,23 +1,23 @@
-<form class="grid" role="form" method="POST" action="{{ route('api.forms.inquiries') }}">
+<form class="grid-1" role="form" method="POST" action="{{ route('api.forms.inquiries') }}">
   {{ csrf_field() }}
   <input type="hidden" name="product" value="">
   <input type="hidden" name="id" value="">
 
-  <div class="col-12 form-control">
+  <div class="col">
     <label class="label" for="name">{{ ucfirst(__('front.name')) }}</label>
-    <input id="name" type="text" class="text-box" name="name" value="{{ old('name') }}" required autofocus placeholder="{{ ucfirst(__('front.name')) }}">
+    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus placeholder="{{ ucfirst(__('front.name')) }}">
 
     @if ($errors->has('name'))
       <div class="invalid-feedback">
-        <strong>{{ $message }}</strong>
+        <strong>jkhhui khug klg ghk yu</strong>
       </div>
     @endif
   </div>
 
-  <div class="col-12 form-control {{ $errors->has('email') ? 'is-invalid' : '' }}">
+  <div class="col {{ $errors->has('email') ? 'is-invalid' : '' }}">
     <div>
       <label class="label" for="email">{{ ucfirst(__('front.email')) }}</label>
-      <input id="email" type="email" class="text-box" name="email" value="{{ old('email') }}" required autofocus placeholder="{{ ucfirst(__('front.email')) }}">
+      <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="{{ ucfirst(__('front.email')) }}">
 
       @if ($errors->has('email'))
         <div class="invalid-feedback">
@@ -27,11 +27,11 @@
     </div>    
   </div>
   
-  <div class="col-12 form-control {{ $errors->has('phone') ? 'is-invalid' : '' }}">     
+  <div class="col {{ $errors->has('phone') ? 'is-invalid' : '' }}">     
 
     <div>
       <label class="label" for="phone">{{ ucfirst(__('front.phone')) }}</label>
-      <input id="phone" type="text" class="text-box" name="phone" value="{{ old('phone') }}" required autofocus placeholder="{{ ucfirst(__('front.phone')) }}">
+      <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" required autofocus placeholder="{{ ucfirst(__('front.phone')) }}">
 
       @if ($errors->has('phone'))
         <div class="invalid-feedback">
@@ -41,11 +41,11 @@
     </div>  
   </div>
   
-  <div class="col-12 form-control {{ $errors->has('departure') ? 'is-invalid' : '' }}">    
+  <div class="col {{ $errors->has('departure') ? 'is-invalid' : '' }}">    
 
     <div>
       <label class="label" for="departure">{{ ucfirst(__('front.departure')) }}</label>
-      <input id="departure" type="text" class="text-box" name="departure" value="{{ old('departure') }}" required autofocus placeholder="{{ ucfirst(__('front.departure')) }}">
+      <input id="departure" type="text" class="form-control" name="departure" value="{{ old('departure') }}" required autofocus placeholder="{{ ucfirst(__('front.departure')) }}">
 
       @if ($errors->has('departure'))
         <div class="invalid-feedback">
@@ -55,50 +55,48 @@
     </div>      
   </div>
   
-  <div class="col-12 grid">  
+  <div class="col grid">  
 
-    <div class="col-6 form-control {{ $errors->has('adults') ? 'is-invalid' : '' }}">    
+    <div class="col-6 {{ $errors->has('adults') ? 'is-invalid' : '' }}">    
 
-      <div>
         <label class="label" for="adults">{{ ucfirst(__('front.adults')) }}</label>
-        <input id="adults" type="text" class="text-box" name="adults" value="{{ old('adults') }}" required autofocus placeholder="{{ ucfirst(__('front.adults')) }}">
+        <input id="adults" type="number" min="0" class="form-control" name="adults" value="{{ old('adults') }}" required autofocus placeholder="{{ ucfirst(__('front.adults')) }}">
 
         @if ($errors->has('adults'))
           <div class="invalid-feedback">
             <strong>{{ $message }}</strong>
           </div>
         @endif
-      </div>      
+
     </div>  
 
-    <div class="col-6 form-control {{ $errors->has('childs') ? 'is-invalid' : '' }}">    
-
-      <div>
+    <div class="col-6 {{ $errors->has('childs') ? 'is-invalid' : '' }}">    
         <label class="label" for="childs">{{ ucfirst(__('front.childs')) }}</label>
-        <input id="childs" type="text" class="text-box" name="childs" value="{{ old('childs') }}" required autofocus placeholder="{{ ucfirst(__('front.childs')) }}">
+        <input id="childs" type="number" min="0" class="form-control" name="childs" value="{{ old('childs') }}" required autofocus placeholder="{{ ucfirst(__('front.childs')) }}">
 
         @if ($errors->has('childs'))
           <div class="invalid-feedback">
             <strong>{{ $message }}</strong>
           </div>
         @endif
-      </div>      
+
     </div>     
 
   </div>
 
-  <div class="col-12 form-control {{ $errors->has('comment') ? 'is-invalid' : '' }}">    
+  <div class="col {{ $errors->has('comment') ? 'is-invalid' : '' }}">    
+    <label class="label" for="comment">{{ ucfirst(__('front.departure')) }}</label>
+    <textarea name="comment" class="form-control textarea"></textarea>
 
-    <div>
-      <label class="label" for="comment">{{ ucfirst(__('front.departure')) }}</label>
-      <textarea name="comment"></textarea>
+    @if ($errors->has('comment'))
+      <div class="invalid-feedback">
+        <strong>{{ $message }}</strong>
+      </div>
+    @endif    
+  </div>
 
-      @if ($errors->has('comment'))
-        <div class="invalid-feedback">
-          <strong>{{ $message }}</strong>
-        </div>
-      @endif
-    </div>      
-  </div>  
+  <div class="col">
+    <input type="submit" class="button button__cta" value="{{ ucfirst(__('front.cta')) }}" />
+  </div>
 
 </form>
--- resources/views/front/widgets/product-inquiries.blade.php
+++ resources/views/front/widgets/product-inquiries.blade.php
@@ -1,3 +1,7 @@
-<div>
-  @include('front.forms.inquiries')
+<div class="overlay">
+  <div class="overlay__wrapper">
+    <div class="overlay__close"></div>
+    <div class="overlay__loading"></div>
+    @include('front.forms.inquiries')
+  </div>
 </div>
