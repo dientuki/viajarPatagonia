ec798a25af1d1905e5f5c90f1e2f74f7273eaec5
Juan <dientuki@gmail.com>
2019-11-03 21:42:43 -0300

    some fixes

---
+++ resources/lang/en/front.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Pagination Language Lines
+  |--------------------------------------------------------------------------
+  |
+  | The following language lines are used by the paginator library to build
+  | the simple pagination links. You are free to change them to anything
+  | you want to customize your views to better match your application.
+  |
+  */
+
+    //Header
+    'go_to_home' => 'Go to the home page',
+    'home' => 'home',
+    'hotels' => 'hotels',
+    'cars' => 'cars',
+    'search' => 'search',
+    'currency' => 'currency',
+    'language' => 'language',
+    'change_to' => 'change to',
+
+    //Footer
+    'afip' => 'Tax',
+
+    //Price
+    'before' => 'before',
+    'now' => 'now',
+    'final' => 'final',
+
+    //Call to action
+    'cta' => 'Call to action',
+
+    //Sidebar
+    'another_packages' => 'another packages',
+
+    //Inquiries
+    'name' => 'name',
+    'email' => 'email',
+    'phone' => 'phone',
+    'departure' => 'departure',
+    'adults' => 'adults',
+    'childs' => 'childs',
+    'comments' => 'comments'
+
+];
