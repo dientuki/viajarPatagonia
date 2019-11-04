0a3cd40a5d42effb7980750c05b250f7db067968
Juan <dientuki@gmail.com>
2019-11-04 00:53:18 -0300

    home ready

--- resources/views/front/product-preview/horizontal.blade.php
+++ resources/views/front/product-preview/horizontal.blade.php
@@ -1,5 +1,5 @@
 @foreach ($products as $product)
-  <article class="list grid col-12">
+  <article class="list list--horizontal grid col-12">
     <?php 
       $images = $product->getMedia('products');
       $img = '';
@@ -9,15 +9,21 @@
       }
       
     ?>
-    <figure class="aspect-preview list__figure col-3">
-      <img src="about:blank" data-original="{{ $img }}" class="lzl" />
-    </figure>
+    <div class="col-3">
+      <figure class="aspect-preview list__figure">
+        <img src="about:blank" data-original="{{ $img }}" class="lzl" />
+      </figure>
+    </div>
     <div class="col">
       <h1 class="list__title">{{ $product->name }}</h1>
+      @if (isset($product->dataExtra))
+        <div class="list__summary">{!! nl2br($product->summary) !!}</div>
+      @endif
+      <div class="list__data-extra bold">Disponible: Todo el año - Duración: Día Completo</div>
       <div class="list__summary">{!! nl2br($product->summary) !!}</div>
     </div>
     <div class="col-3">
-      <div class="button button__price">{{ $product->getPrice() }}</div>
+      <div class="button button__price">{{ $product->getPrice() }} {{__('front.final')}}</div>
     </div>
   </article>
 @endforeach
