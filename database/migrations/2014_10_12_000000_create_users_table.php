c00494466a1750a7fdf7ad4582654adf6f5c657b
Juan <dientuki@gmail.com>
2019-11-08 23:35:25 -0300

    update db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 60);
+            $table->string('password', 60)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
