0b765d4365b6aa958a4062435e0ae3265079f494
Juan <dientuki@gmail.com>
2019-11-10 01:13:37 -0300

    update controller

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Request;
 
 class ImagesController extends Controller
 {
-    public function getClientHint($id, $slug, $ext) {
+    public function getClientHint($id, $image) {
       $width = Request::header('Width');
       $dpr = is_int(Request::header('DPR')) ? Request::header('DPR') : 1;
 
@@ -19,7 +19,7 @@ class ImagesController extends Controller
       }
 
       $path = storage_path('app/public/' . $id . '/ch/');
-      $filename = $path . $dpr . '-' . $width . '.' . $ext;
+      $filename = $path . $dpr . '-' . $width . '.' . $this->file_ext($image);
       
       if (file_exists($filename) == false) {
         if (is_dir($path) == false) {
@@ -31,5 +31,9 @@ class ImagesController extends Controller
       }
       
       return response()->file($filename);
+    }
+
+    private function file_ext($filename) {
+      return preg_match('/\./', $filename) ? preg_replace('/^.*\./', '', $filename) : '';
     }
 }
