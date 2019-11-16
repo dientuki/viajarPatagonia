dc395ca6006b3ad8326abde98afa0f4f554479cb
Juan <dientuki@gmail.com>
2019-11-16 10:33:52 -0300

    added Availability tables

---
+++ database/migrations/2019_11_16_132203_create_availability_translation_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAvailabilityTranslationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('availability_translation', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+
+            $table->smallIncrements('id');
+            $table->tinyInteger('fk_language')->unsigned();
+            $table->tinyInteger('fk_availability')->unsigned();
+            $table->string('availability');
+
+            $table->foreign('fk_language')->references('id')->on('languages');
+            $table->foreign('fk_availability')->references('id')->on('availability');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('availability_translation');
+    }
+}
