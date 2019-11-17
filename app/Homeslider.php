bd17cb18137e40dc5485a317a974e0c0598f64c3
Juan <dientuki@gmail.com>
2019-11-17 10:29:05 -0300

    controller

--- app/Homeslider.php
+++ app/Homeslider.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Homesliders extends Model implements HasMedia
+class Homeslider extends Model implements HasMedia
 {
   use HasMediaTrait;
     /**
@@ -18,7 +18,7 @@ class Homesliders extends Model implements HasMedia
      *
      * @var string
      */
-    protected $table = 'homesliders';
+    protected $table = 'homeslider';
 
     /**
      * Indicates if the model should be timestamped.
@@ -32,26 +32,26 @@ class Homesliders extends Model implements HasMedia
      *
      * @var array
      */
-    protected $fillable = ['is_active', 'map', 'fk_excursion_type', 'fk_destination', 'fk_availability', 'fk_duration'];    
+    protected $fillable = ['is_active', 'url', 'hotel', 'stars', 'order'];
 
     static public function getAll(){
-      $homesliders = Homesliders::select('homesliders.id', 'homesliders.is_active');
+      $homeslider = Homesliders::select('homeslider.id', 'homeslider.is_active');
       $languages = Language::getAll();
 
       foreach ($languages as $language) {
-        $homesliders->addSelect("ct$language->id.title as title$language->id")
-          ->join("homesliders_translation as ct$language->id", 'homesliders.id', '=', "ct$language->id.fk_homeslider")
+        $homeslider->addSelect("ct$language->id.title as title$language->id")
+          ->join("homeslider_translation as ct$language->id", 'homeslider.id', '=', "ct$language->id.fk_homeslider")
           ->join("languages as l$language->id", "l$language->id.id", '=', "ct$language->id.fk_language")
           ->where("l$language->id.iso", $language->iso);
       }
 
-      return $homesliders->get();
+      return $homeslider->orderBy('homeslider.order')->get();
     }
 
     static function getEdit($id){
 
-      $homesliders = homesliders::select('id', 'is_active', 'map', 'fk_excursion_type', 'fk_destination', 'fk_availability', 'fk_duration');
-      $result = $homesliders->where('id', $id)->get()->first();
+      $homeslider = homeslider::select('id', 'is_active', 'url', 'hotel', 'stars', 'order');
+      $result = $homeslider->where('id', $id)->get()->first();
   
       if (is_array($id)) {
         if (count($result) == count(array_unique($id))) {
--- database/seeds/HomesliderTranslationSeeder.php
+++ database/seeds/HomesliderTranslationSeeder.php
@@ -13,42 +13,42 @@ class HomesliderTranslationSeeder extends Seeder
     {
         DB::table('homeslider_translation')->insert([
             'fk_language' => 1,
-            'fk_homeslider' => 1,
+            'fk_slider' => 1,
             'title' => 'Patagonia excursion  es',
             'date' => 'Salida',
             'description' => 'bajada patagonia',
         ]);
         DB::table('homeslider_translation')->insert([
             'fk_language' => 2,
-            'fk_homeslider' => 1,
+            'fk_slider' => 1,
             'title' => 'Patagonia excursion  en',
             'date' => 'Salida',
             'description' => 'description patagonia',
         ]);
         DB::table('homeslider_translation')->insert([
             'fk_language' => 3,
-            'fk_homeslider' => 1,
+            'fk_slider' => 1,
             'title' => 'Patagonia excursion  pt',
             'date' => 'Salida',
             'description' => 'linha de queda patagonia',
         ]);
         DB::table('homeslider_translation')->insert([
             'fk_language' => 1,
-            'fk_homeslider' => 2,
+            'fk_slider' => 2,
             'title' => 'Antartida excursion  es',
             'date' => 'Salida',
             'description' => 'bajada Antartida excursion ',            
         ]);
         DB::table('homeslider_translation')->insert([
             'fk_language' => 2,
-            'fk_homeslider' => 2,
+            'fk_slider' => 2,
             'title' => 'Antartida excursion  en',
             'date' => 'Salida',
             'description' => 'description Antartida excursion ',  
         ]);
         DB::table('homeslider_translation')->insert([
             'fk_language' => 3,
-            'fk_homeslider' => 2,
+            'fk_slider' => 2,
             'title' => 'Antartida excursion  pt',
             'date' => 'Salida',
             'description' => 'linha de queda Antartida excursion ',  
