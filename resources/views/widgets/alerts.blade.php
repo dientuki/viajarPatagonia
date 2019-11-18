42acb570954e0abdbeef1eebe97f399e93a8c2bd
Juan <dientuki@gmail.com>
2019-11-18 18:24:15 -0300

    html

--- resources/views/widgets/alerts.blade.php
+++ resources/views/widgets/alerts.blade.php
@@ -1,11 +1,9 @@
+<?php use App\Http\Helpers\Helpers; ?>
+
 @foreach (Alert::getMessages() as $type => $messages)
-  <div class="alert-wrapper">
+  <div class="alert-wrapper animated zoomIn">
     @foreach ($messages as $message)
-      <div class="alert-animation animated zoomIn">
-        <div class="alert-fix">
-          <div class="alert alert-{{ $type }}"><strong>{{ ucfirst(trans('alerts.' . $type)) }}!</strong> {{ $message }} <span class="alert-close"></span></div>
-        </div>
-      </div>
+      <div class="alert alert-{{ $type }}"><strong>{{ ucfirst(trans('alerts.' . $type)) }}!</strong> {{ $message }} <span class="alert-close">{!! Helpers::load_svg('ico-close') !!}</span></div>
     @endforeach
   </div>
 @endforeach
