a76658ef4a004def46e5da44a5f2f911ff1edf0a
Juan <dientuki@gmail.com>
2019-12-06 10:27:52 -0300

    update form

--- resources/views/front/forms/inquiries.blade.php
+++ resources/views/front/forms/inquiries.blade.php
@@ -5,25 +5,21 @@
 
   <div class="col">
     <label class="label" for="name">{{ ucfirst(__('front.name')) }}</label>
-    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus placeholder="{{ ucfirst(__('front.name')) }}">
+    <input id="name" type="text" class="form-control" name="name" autofocus placeholder="{{ ucfirst(__('front.name')) }}">
 
-    @if ($errors->has('name'))
-      <div class="invalid-feedback">
-        <strong>{{ $errors->first('name') }}</strong>
-      </div>
-    @endif
+    <div class="invalid-feedback">
+      <strong></strong>
+    </div>
   </div>
 
   <div class="col {{ $errors->has('email') ? 'is-invalid' : '' }}">
     <div>
       <label class="label" for="email">{{ ucfirst(__('front.email')) }}</label>
-      <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="{{ ucfirst(__('front.email')) }}">
+      <input id="email" type="email" class="form-control" name="email" required placeholder="{{ ucfirst(__('front.email')) }}">
 
-      @if ($errors->has('email'))
-        <div class="invalid-feedback">
-          <strong>{{ $errors->first('email') }}</strong>
-        </div>
-      @endif
+      <div class="invalid-feedback">
+        <strong></strong>
+      </div>
     </div>    
   </div>
   
@@ -31,13 +27,11 @@
 
     <div>
       <label class="label" for="phone">{{ ucfirst(__('front.phone')) }}</label>
-      <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" required autofocus placeholder="{{ ucfirst(__('front.phone')) }}">
+      <input id="phone" type="text" class="form-control" name="phone" required placeholder="{{ ucfirst(__('front.phone')) }}">
 
-      @if ($errors->has('phone'))
-        <div class="invalid-feedback">
-          <strong>{{ $errors->first('phone') }}</strong>
-        </div>
-      @endif
+      <div class="invalid-feedback">
+        <strong></strong>
+      </div>
     </div>  
   </div>
   
@@ -45,13 +39,11 @@
 
     <div>
       <label class="label" for="departure">{{ ucfirst(__('front.departure')) }}</label>
-      <input id="departure" type="text" class="form-control" name="departure" value="{{ old('departure') }}" required autofocus placeholder="31/12/2012">
+      <input id="departure" type="text" class="form-control" name="departure" required placeholder="31/12/2012">
 
-      @if ($errors->has('departure'))
-        <div class="invalid-feedback">
-          <strong>{{ $errors->first('departure') }}</strong>
-        </div>
-      @endif
+      <div class="invalid-feedback">
+        <strong></strong>
+      </div>
     </div>      
   </div>
   
@@ -60,25 +52,21 @@
     <div class="col-6 {{ $errors->has('adults') ? 'is-invalid' : '' }}">    
 
         <label class="label" for="adults">{{ ucfirst(__('front.adults')) }}</label>
-        <input id="adults" type="number" min="0" max="10" class="form-control" name="adults" value="{{ old('adults') }}" required autofocus placeholder="{{ ucfirst(__('front.adults')) }}">
+        <input id="adults" type="number" min="0" max="10" class="form-control" name="adults" required placeholder="{{ ucfirst(__('front.adults')) }}">
 
-        @if ($errors->has('adults'))
-          <div class="invalid-feedback">
-            <strong>{{ $errors->first('adults') }}</strong>
-          </div>
-        @endif
+        <div class="invalid-feedback">
+          <strong></strong>
+        </div>
 
     </div>  
 
     <div class="col-6 {{ $errors->has('childs') ? 'is-invalid' : '' }}">    
         <label class="label" for="childs">{{ ucfirst(__('front.childs')) }}</label>
-        <input id="childs" type="number" min="0" max="10" class="form-control" name="childs" value="{{ old('childs') }}" required autofocus placeholder="{{ ucfirst(__('front.childs')) }}">
+        <input id="childs" type="number" min="0" max="10" class="form-control" name="childs" required placeholder="{{ ucfirst(__('front.childs')) }}">
 
-        @if ($errors->has('childs'))
-          <div class="invalid-feedback">
-            <strong>{{ $errors->first('childs') }}</strong>
-          </div>
-        @endif
+        <div class="invalid-feedback">
+          <strong></strong>
+        </div>
 
     </div>     
 
@@ -88,11 +76,9 @@
     <label class="label" for="comment">{{ ucfirst(__('front.comments')) }}</label>
     <textarea name="comment" class="form-control textarea"></textarea>
 
-    @if ($errors->has('comment'))
-      <div class="invalid-feedback">
-        <strong>{{ $errors->first('comment') }}</strong>
-      </div>
-    @endif    
+    <div class="invalid-feedback">
+      <strong></strong>
+    </div>
   </div>
 
   <div class="col">
