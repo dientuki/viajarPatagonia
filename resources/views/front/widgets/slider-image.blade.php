f33bae237e6c9b40b09c4f9f1f427270b2865bd0
Juan <dientuki@gmail.com>
2019-12-22 09:27:46 -0300

    title

--- resources/views/front/widgets/slider-image.blade.php
+++ resources/views/front/widgets/slider-image.blade.php
@@ -6,8 +6,8 @@
 
 <div class="header-slider__content">
   <div class="header-slider__title header-slider__top">{{ $slide->title }}</div>
-  <div class="header-slider__date">{{ $slide->date }}</div>
-  <div class="header-slider__description header-slider__top">{{ $slide->description }}</div>
+  <div class="header-slider__date">{!! nl2br($slide->date) !!}</div>
+  <div class="header-slider__description header-slider__top">{!! nl2br($slide->description) !!}</div>
   <div class="header-slider__hotel header-slider__top">{{ $slide->hotel }} {{ $slide->stars }} <span class="star"></span></div>
   <?php $url = Helpers::slider_get_url($slide->url); ?>
   @if ($url != false)
