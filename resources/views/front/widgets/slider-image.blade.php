8a6963930567788a87a9904e447dd1cb5552ea3a
Juan <dientuki@gmail.com>
2019-12-22 09:18:50 -0300

    added link

--- resources/views/front/widgets/slider-image.blade.php
+++ resources/views/front/widgets/slider-image.blade.php
@@ -1,11 +1,16 @@
-  <figure class="aspect-homeslider">
-    <img data-src="{{ $image->getFullUrl('slider_desktop') }}" class="{{ $lazyload }}"/>
-  </figure>
+<?php use App\Http\Helpers\Helpers; ?>
 
-  <div class="header-slider__content">
-    <div class="header-slider__title header-slider__top">{{ $slide->title }}</div>
-    <div class="header-slider__date">{{ $slide->date }}</div>
-    <div class="header-slider__description header-slider__top">{{ $slide->description }}</div>
-    <div class="header-slider__hotel header-slider__top">{{ $slide->hotel }} {{ $slide->stars}}*</div>
-    <div class="header-slider__cta header-slider__top button">{{ ucfirst(__('front.cta')) }}</div>
-  </div>
+<figure class="aspect-homeslider">
+  <img data-src="{{ $image->getFullUrl('slider_desktop') }}" class="{{ $lazyload }}"/>
+</figure>
+
+<div class="header-slider__content">
+  <div class="header-slider__title header-slider__top">{{ $slide->title }}</div>
+  <div class="header-slider__date">{{ $slide->date }}</div>
+  <div class="header-slider__description header-slider__top">{{ $slide->description }}</div>
+  <div class="header-slider__hotel header-slider__top">{{ $slide->hotel }} {{ $slide->stars }} <span class="star"></span></div>
+  <?php $url = Helpers::slider_get_url($slide->url); ?>
+  @if ($url != false)
+    <a class="header-slider__cta header-slider__top button" href="{{ $url }}">{{ ucfirst(__('front.cta')) }}</a>
+  @endif
+</div>
