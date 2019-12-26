577dc69f7e3e1431a7c436021bd4480715bf09f1
Juan <dientuki@gmail.com>
2019-12-25 23:04:28 -0300

    added anchor in call

--- resources/views/front/product-preview/vertical.blade.php
+++ resources/views/front/product-preview/vertical.blade.php
@@ -16,7 +16,7 @@
       <img src="about:blank" data-original="{{ $img }}" class="lzl" />
     </figure>
       <h1 class="list__title bold">
-        <a href="{{route($route, $routeParams)}}" class="list_link">{{ $product->name }}</a>
+        <a href="{{route($route, $routeParams)}}#product" class="list_link">{{ $product->name }}</a>
       </h1>
     <div class="list__summary">{!! nl2br($product->summary) !!}</div>
     <div class="button button__price">{{ $product->getPrice() }} {{__('front.final')}}</div>
