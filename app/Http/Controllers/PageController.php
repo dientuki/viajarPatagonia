aa40691d9168fc89866427abf54fa2dcca25ae35
Juan <dientuki@gmail.com>
2019-12-29 01:39:17 -0300

    added controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Currency;
+use App\Packages;
+use App\Excursions;
+use App\Cruiseships;
+use App\Http\Helpers\Helpers;
+use App\Translations\Language;
+use App\Http\Controllers\Controller;
+
+class pagesController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function show()
+    {
+
+    }
+}
