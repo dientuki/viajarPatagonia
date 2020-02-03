a4633fad2aa1cf50bc3854bf8d429f5075975be5
Juan <dientuki@gmail.com>
2020-02-03 09:51:56 -0300

    added lang file

---
+++ resources/lang/pt/front.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Pagination Language Lines
+  |--------------------------------------------------------------------------
+  |
+  | The following language lines are used by the paginator library to build
+  | the simple pagination links. You are free to change them to anything
+  | you want to customize your views to better match your application.
+  |
+  */
+
+    //Header
+    'go_to_home' => 'Ir a la página principal',
+    'home' => 'inicio',
+    'hotels' => 'hoteles',
+    'cars' => 'autos',
+    'search' => 'buscar',
+    'currency' => 'moneda',
+    'language' => 'idioma',
+    'change_to' => 'cambiar a',
+
+    //Footer
+    'afip' => 'Datos fiscales',
+
+    //Price
+    'before' => 'antes',
+    'now' => 'ahora',
+    'final' => 'final',
+
+    //Call to action
+    'cta' => 'consultar',
+
+    //Sidebar
+    'another_package' => 'otros paquetes',
+    'another_excursion' => 'otras excursiones',
+    'another_cruise' => 'otros cruceros',
+    'detail_package' => 'package detail',
+    'detail_excursion' => 'excursin detail',
+    'detail_cruise' => 'cruise detail',
+
+    //Inquiries
+    'name' => 'nombre',
+    'email' => 'email',
+    'phone' => 'teléfono',
+    'departure' => 'fecha de salida',
+    'adult' => 'adultos',
+    'child' => 'infantes',
+    'comments' => 'comentarios',    
+
+    //Home
+    'packages' => 'paquetes',
+    'cruiseships' => 'cruceros',
+    'excursions' => 'excursiones',
+    'view-all' => 'ver todos',
+    
+    //Buy more
+    'buy-more-excursion' => 'podes contratar estas excursiones extra',
+    'included-excursion' => 'excursiones incluidas',
+
+    // Disponibilidad
+    'availability' => 'disponible',
+    'duration' => 'duración',
+];
